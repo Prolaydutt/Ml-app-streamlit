52385a04bc9c166ebd07d157c8d53d48333a690e
Prolay Dutta <141430706+Prolaydutt@users.noreply.github.com>
2025-12-05 13:08:29 +0530

    Change model file name to 'hog_cnn_model.h5'

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Load the trained model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("model.h5")
+    model = tf.keras.models.load_model("hog_cnn_model.h5")
     return model
 
 model = load_model()
